747a36c0eb6172b1dce7e141e87daa9b9f60a6ab
alex 陈 <1342445953@qq.com>
2016-01-09 15:45:52 +0800

    Initial Commit

---
+++ 通讯录/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  通讯录
+//
+//  Created by 陈一根陈一根 on 16/1/9.
+//  Copyright © 2016年 陈一根. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
